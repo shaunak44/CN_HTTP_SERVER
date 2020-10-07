71634f9db4ccf9fb42bf38b9125e20ad92a9b605
shaunak44 <shaunakmahajan44@gmail.com>
2020-10-07 21:06:44 +0530

    spaces

--- temp.py
+++ temp.py
@@ -1,4 +1,38 @@
-import mimetypes
-t = mimetypes.MimeTypes().guess_type('index.html')[0]
-print(type(t), type("asda"))
-
+import thread 
+import os 
+  
+def task1():
+	while True:
+		try: 
+			print("Task 1 assigned to thread: {}".format(threading.current_thread().name)) 
+			print("ID of process running task 1: {}".format(os.getpid())) 
+		except:
+			break
+  
+def task2(): 
+	while True:
+		try:
+			print("Task 2 assigned to thread: {}".format(threading.current_thread().name)) 
+			print("ID of process running task 2: {}".format(os.getpid()))
+		except:
+			break 
+  
+if __name__ == "__main__": 
+  
+    # print ID of current process 
+    print("ID of process running main program: {}".format(os.getpid())) 
+  
+    # print name of main thread 
+    print("Main thread name: {}".format(threading.current_thread().name)) 
+  
+    # creating threads 
+    t1 = threading.Thread(target=task1, name='t1') 
+    t2 = threading.Thread(target=task2, name='t2')   
+  
+    # starting threads 
+    t1.start() 
+    t2.start() 
+  
+    # wait until all threads finish 
+    #t1.join() 
+    #t2.join() 
