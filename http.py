9cf7e9eac2cfa3c1503a7ed5bde7884c6602ffa0
shaunak44 <shaunakmahajan44@gmail.com>
2020-10-20 18:49:11 +0530

    304 status_code added

--- http.py
+++ http.py
@@ -17,29 +17,6 @@ serverSocket.bind(('', portNumber))
 serverSocket.listen(20)
 print("HTTP server running on port ", portNumber)
 
-'''def recv_timeout(the_socket,timeout=0.3):
-	the_socket.setblocking(0) 
-	total_data=[];
-	data=b'';
-	begin=time.time()
-	while 1:
-		if total_data and time.time()-begin > timeout:
-			break
-		
-		elif time.time()-begin > timeout*2:
-			break	
-		try:
-			data = the_socket.recv(8192)
-			if data:
-				total_data.append(data.decode())
-				begin=time.time()
-			else:
-				time.sleep(0.1)
-		except:
-			pass
-	
-	return ''.join(total_data)'''
-
 def recv_timeout(socket):
 	BUFF_SIZE = 4096
 	data = b''
@@ -54,7 +31,11 @@ def recv_timeout(socket):
 def date():
 	now = datetime.now()
 	stamp = mktime(now.timetuple())
-	return format_date_time(stamp)
+	return time.asctime(time.localtime())
+
+def date_mktime(string):
+	return time.strptime(string, "%a %b %d %H:%M:%S %Y")
+
 
 def parse_body(data):
 	_, body = data.split("\r\n\r\n")
@@ -69,13 +50,18 @@ def parse_headers(words):
 		headers[words[i][0][:-1]] = words[i][1]
 	return headers
 
-def create_header(header, Content_len = 0, Content_type = "text/html", method = "GET"):
+def create_header(header, Content_len = 0, Content_type = "text/html", method = "GET", last_mod_date = None):
 	header += ("Date: " + date()+"\r\n")
 	header += ("Server: Http server (ubuntu)\r\n")
 	header += ("Content-Length: " + str(Content_len) + "\r\n")
 	header += ("Connection: Close\r\n")
 	header += ("Content-Type: " + str(Content_type) + "\r\n")
 	if(method == "GET"):
+		if(last_mod_date != None):
+			try:
+				header += ("Last-Modified: " + str(last_mod_date) + "\r\n")
+			except Exception as e:
+				print(e)
 		header += ("\r\n")
 	#print(header)	
 	return header
@@ -107,7 +93,7 @@ def client_thread(clientSocket):
 			#print(requestWords)
 			headers = parse_headers(requestWords)
 			requestBody = parse_body(requestData)
-			#print(headers)
+			print(headers)
 			#print(requestBody)
 			flag_status_code = {"200": False, "304": False, "400": False, "404": False, "201": False, "204": False}
 			if(requestWords[0][0] == "GET"):
@@ -148,12 +134,29 @@ def client_thread(clientSocket):
 					fileObject = requestedFile.read()
 					requestedFileLen = len(fileObject)
 					#print(requestedFileLen)
-					responseHeader += (" 200 " + status_code["200"] + "\r\n")
-					responseHeader = create_header(responseHeader, requestedFileLen, requestedFileType)
-					#print(responseHeader)
-					fileObject = responseHeader.encode() + fileObject
-					#print(fileObject)
-					clientSocket.sendall(fileObject)
+					if(headers.get("If-Modified-Since", None)):
+						print(date())
+						Request_date = date_mktime(headers["If-Modified-Since"])
+						Current_date = date_mktime(date())
+						File_mod_date = path.getmtime(url)
+						if(mktime(Request_date) - mktime(Current_date) < File_mod_date):
+							flag_status_code["304"] = True
+							print("Not Modified")
+						else:
+							flag_status_code["200"] = True
+
+					if(flag_status_code["304"]):
+						responseHeader += (" 304 " + status_code["304"] + "\r\n")
+						responseHeader = create_header(responseHeader, requestedFileLen, requestedFileType,"GET" ,time.ctime(path.getmtime(url)))
+						clientSocket.sendall(responseHeader.encode())
+					else:
+						responseHeader += (" 200 " + status_code["200"] + "\r\n")
+						responseHeader = create_header(responseHeader, requestedFileLen, requestedFileType,"GET" ,time.ctime(path.getmtime(url)))
+						#print(time.ctime(path.getmtime(url)))
+						#print(responseHeader)
+						fileObject = responseHeader.encode() + fileObject
+						#print(fileObject)
+						clientSocket.sendall(fileObject)
 
 			elif (requestWords[0][0] == "POST"):
 				print("Executing post....")	
@@ -211,3 +214,26 @@ while True:
 		print("\n*****Http server stopped*****")
 		break
 serverSocket.close()
+'''def recv_timeout(the_socket,timeout=0.3):
+	the_socket.setblocking(0) 
+	total_data=[];
+	data=b'';
+	begin=time.time()
+	while 1:
+		if total_data and time.time()-begin > timeout:
+			break
+		
+		elif time.time()-begin > timeout*2:
+			break	
+		try:
+			data = the_socket.recv(8192)
+			if data:
+				total_data.append(data.decode())
+				begin=time.time()
+			else:
+				time.sleep(0.1)
+		except:
+			pass
+	
+	return ''.join(total_data)'''
+
